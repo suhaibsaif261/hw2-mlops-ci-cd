8cc05416b619c8941898017a763f30b1cfea8b47
suhaibsaif261 <suhaibsaif261@gmail.com>
2026-01-11 21:00:04 +0300

    Break unit test to demonstrate stop-the-line

--- mlops-demo/tests/test_features.py
+++ mlops-demo/tests/test_features.py
@@ -4,7 +4,7 @@ from src.features import text_to_number
 
 
 def test_text_to_number_empty():
-    assert text_to_number("") == 0
+    assert text_to_number("") == 999
 
 
 def test_text_to_number_known():
